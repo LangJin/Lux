85384545450e0c2bb669fdec16a9cc83afda7d25
SNake <snake_goodluck@hotmail.com>
2018-03-20 17:34:00 +0800

    添加date操作工具

--- common/util_date.py
+++ common/util_date.py
@@ -7,6 +7,6 @@ from datetime import datetime
     获取当前时间
     renturn ："2018-03-20 17:30:56"
 """
-def get_datetime_now():
+def get_current_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
