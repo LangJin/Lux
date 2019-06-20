03708aeed5ad9f0431269225633022e40d78cb9e
langjin <fenyukuang@163.com>
2019-06-20 18:07:28 +0800

    登录账号为空

--- app/users/user.py
+++ app/users/user.py
@@ -16,27 +16,32 @@ def userlogin():
     userinfo = request.get_json()
     username = userinfo.get("username")
     password = userinfo.get("password")
-    sql = "select * from t_user where username = '{}'".format(username)
-    res = query(sql)
-    data = {}
-    if len(res) != 1:
-        data["msg"] = "数据异常或者用户不存在！"
-        data["data"] = None
-    else:
-        password = encryption(username,password,"user")
-        if password == res[0].get("password"):
-            token = create_token()
-            sql = "update t_user set token = '{token}' where id = {id};".format(token=token,id=res[0].get("id"))
-            msg = commit(sql)
-            session.clear()
-            session["username"] = username
-            session["token"] = token
-            data["msg"] = "登录成功！"
-            data["data"] = {}
-            data["data"]["token"] = token
-            data["data"]["status"] = msg
+    if len(username) != 0 and len(password) != 0:
+        sql = "select * from t_user where username = '{}'".format(username)
+        res = query(sql)
+        data = {}
+        if len(res) != 1:
+            data["msg"] = "数据异常或者用户不存在！"
+            data["data"] = None
         else:
-            data["msg"] = "密码错误！"
+            password = encryption(username,password,"user")
+            if password == res[0].get("password"):
+                token = create_token()
+                sql = "update t_user set token = '{token}' where id = {id};".format(token=token,id=res[0].get("id"))
+                msg = commit(sql)
+                session.clear()
+                session["username"] = username
+                session["token"] = token
+                data["msg"] = "登录成功！"
+                data["data"] = {}
+                data["data"]["token"] = token
+                data["data"]["status"] = msg
+            else:
+                data["msg"] = "密码错误！"
+    else:
+        data = {
+            "msg":"账号为空！"
+        }
     return jsonify(data)
 
 
